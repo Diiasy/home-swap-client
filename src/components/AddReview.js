1a33819b39ac1fd71bd241fd1d8930288da8c609
Nienke0905 <nienkewestdijk@gmail.com>
2020-07-02 16:28:56 +0200

    add review

---
+++ src/components/AddReview.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { getUser } from "../utils/auth";
+import qs from 'qs';
+import axios from 'axios';
+
+
+
+export default class AddReview extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.addReview = this.addReview.bind(this);
+    }
+
+    state = {
+        review:  {},
+        error: null
+    }
+
+
+    currentUser = getUser();
+
+    componentDidMount(){
+        let review = {...this.state.review};
+        review[`profile_id`] = this.props.user._id;
+        review[`user_id`] = this.currentUser._id;
+        this.setState({review});
+    }
+
+    handleChange(e) {
+        let review = {...this.state.review};
+        review[e.target.name] = e.target.value;
+        this.setState({review});
+    }
+
+    addReview(e) {
+        e.preventDefault();
+        axios({
+            url: `${process.env.REACT_APP_BASE_URL}/user/review/create`,
+            data: qs.stringify(this.state.review),
+            withCredentials: true,
+            method: "POST"
+        })
+        .then(response => {
+            this.props.profileUpdate(response.data);
+            this.props.history.push(`/user/profile/${response.data._id}`);
+        })
+        .catch(error => {
+            this.setState({ error });
+        });
+    }
+
+    render() {
+        return (
+            <div className="signup">
+                <form className="container">
+                    <div className="form-group">
+                        <label for="content">Review</label>
+                        <input className="form-control" type="text" onChange={this.handleChange} name="content" placeholder="content" />
+                    </div>
+                    <div className="form-group">
+                        <label for="score">Score</label>
+                        <input type="range" name="score" class="form-control-range range" min="1" max="10" onChange={this.handleChange} />
+                        <output class="bubble"></output>                            
+                    </div>
+                    <button onClick={this.addReview} type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,27 +2,19 @@ import React, { Component } from 'react';
 
 class ProfileCard extends Component {
     render() {
-        let pictures = this.props.currentProfile.pictures;
+        let pictures = this.props.user.pictures;
         return(
             <div>
                 <h1>Home details</h1>
-<<<<<<< HEAD
                 <p><strong>Home name: </strong>{this.props.user.homeName}</p>
                 <p><strong>Home location: </strong>{this.props.user.city}</p>
                 <p><strong>Description: </strong>{this.props.user.homeDescription}</p>
                 <p><strong>Owner: </strong>{this.props.user.name}</p>
-=======
-                <p><strong>Home name: </strong>{this.props.currentProfile.homeName}</p>
-                <p><strong>Home location: </strong>{this.props.currentProfile.city}</p>
-                <p><strong>Description: </strong>{this.props.currentProfile.homeDescription}</p>
-                <p><strong>Owner: </strong>{this.props.currentProfile.name}</p>
                 {
                     pictures && pictures.map(picture => <img key={picture._id} src={picture.path} alt={picture.name}/>)
                 }
->>>>>>> 90894d34b40de3d30c1e5d92b490d1d6543f0e74
             </div>
         )
     }
 }
-
 export default ProfileCard;
